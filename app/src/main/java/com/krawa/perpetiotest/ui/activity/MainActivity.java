210799fb5f4ba71e4af3780bf34dc587a321aad2
krawa <kraw4enko@bigmir.net>
2017-01-24 17:56:11 +0200

    added dependencies

---
+++ app/src/main/java/com/krawa/perpetiotest/ui/activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.krawa.perpetiotest.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.krawa.perpetiotest.R;
+import com.krawa.perpetiotest.ui.fragment.NewsFeedFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new NewsFeedFragment(), NewsFeedFragment.TAG)
+                    .commit();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/krawa/perpetiotest/ui/fragment/NewsFeedFragment.java
@@ -0,0 +1,9 @@
+package com.krawa.perpetiotest.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class NewsFeedFragment extends Fragment {
+
+    public static final String TAG = "NewsFeedFragment";
+
+}
